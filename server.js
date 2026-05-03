78606b63161d59a49cec382deb979a609f246007
hali619 <hali0619@gmail.com>
2026-05-03 16:11:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ async function generateCyberFace(base64Image) {
   `;
 
   const response = await ai.models.generateContent({
-    model: 'gemini-2.5-flash-preview-05-20',
+    model: 'gemini-2.5-flash-image',
     contents: {
       parts: [
         { text: prompt },
